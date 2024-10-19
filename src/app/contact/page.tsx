4d51931ca5ec9196055973626be896f97e77442c
saurabh-dev-vns <saurabhsahnai0001@gmail.com>
2024-10-19 21:48:06 +0530

    contact page added

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,63 +4,84 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8">
-      <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-4 text-center">
-        Contact Us
-      </h1>
-      <p className="text-base sm:text-lg text-gray-700 mb-8 text-center">
-        We&apos;d love to hear from you!
-      </p>
-      <form className="w-full max-w-md bg-white shadow-lg p-6 rounded-md">
-        <div className="mb-4">
-          <label
-            className="block text-sm font-medium text-gray-700"
-            htmlFor="name"
-          >
-            Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
-            placeholder="Your name"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            className="block text-sm font-medium text-gray-700"
-            htmlFor="email"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
-            placeholder="Your email"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            className="block text-sm font-medium text-gray-700"
-            htmlFor="message"
-          >
-            Message
-          </label>
-          <textarea
-            id="message"
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
-            placeholder="Your message"
-            rows={4}
-          ></textarea>
-        </div>
-        <button
-          type="submit"
-          className="w-full bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded-md transition-colors"
+    <div>
+      <a
+        className="absolute-top-4 start-0 hidden p-3 text-gray-500 hover:text-gray-700 lg:flex lg:items-center 2xl:-top-7 2xl:ps-20 "
+        href="/"
+      >
+        <svg
+          stroke="currentColor"
+          fill="currentColor"
+          strokeWidth="0"
+          viewBox="0 0 256 256"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          Submit
-        </button>
-      </form>
+          <path d="M228,128a12,12,0,0,1-12,12H69l51.52,51.51a12,12,0,0,1-17,17l-72-72a12,12,0,0,1,0-17l72-72a12,12,0,0,1,17,17L69,116H216A12,12,0,0,1,228,128Z"></path>
+        </svg>
+        <b className="ms-1 font-medium">Back to home</b>
+      </a>
+      <a className="sticky start-0 top-0 z-20 flex items-center justify-center bg-blue-500 p-3.5 text-sm font-medium text-white md:p-4 lg:hidden" href="/"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-arrow-left"><path d="m12 19-7-7 7-7"></path><path d="M19 12H5"></path></svg><p className="font-normal ms-1 font-lexend">Back to home</p></a>
+      <div className="min-h-screen flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8">
+
+        <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-4 text-center">
+          Contact Us
+        </h1>
+        <p className="text-base sm:text-lg text-gray-700 mb-8 text-center">
+          We&apos;d love to hear from you!
+        </p>
+        <form className="w-full max-w-md bg-white shadow-lg p-6 rounded-md">
+          <div className="mb-4">
+            <label
+              className="block text-sm font-medium text-gray-700"
+              htmlFor="name"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+              placeholder="Your name"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-sm font-medium text-gray-700"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+              placeholder="Your email"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-sm font-medium text-gray-700"
+              htmlFor="message"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+              placeholder="Your message"
+              rows={4}
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded-md transition-colors"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
